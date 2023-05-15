932cf0b235e10fdeff20c6d13f2bb06fd52ff454
gazisu <info.gazisu@gmail.com>
2023-05-15 22:06:26 +0600

    Last day end class at BITM

--- src/main/java/MainDemo.java
+++ src/main/java/MainDemo.java
@@ -1,20 +1,75 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class MainDemo {
 
-	@Test
+	@BeforeMethod
+	void setpu() {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Robi\\Documents\\Drivers\\chromedriver.exe");
+			
+	}
+	@Test()
 	
 	
 	void runOnCrome() throws InterruptedException  {
-		System.setProperty("Webdrive.chrome.driver", "C:\\Users\\Robi\\Documents\\Drivers\\chromedriver.exe");
 		WebDriver driver= new ChromeDriver();
-		driver.get("https://www.google.com/");
-		
+		driver.get("https://www.saucedemo.com/");
+		driver.findElement(By.id("user-name")).sendKeys("standard_user");
+		driver.findElement(By.name("password")).sendKeys("secret_sauce");
+		driver.findElement(By.id("login-button")).click();
 		Thread.sleep(2000);
-//		driver.close();
+		driver.close();
 		}
-
+	@Test()
 	
+	
+	void runOnCrome1() throws InterruptedException  {
+		WebDriver driver= new ChromeDriver();
+		driver.get("https://www.saucedemo.com/");
+		driver.findElement(By.id("user-name")).sendKeys("locked_out_user");
+		driver.findElement(By.name("password")).sendKeys("secret_sauce");
+		driver.findElement(By.id("login-button")).click();
+		Thread.sleep(2000);
+		driver.close();
+		}
+	@Test()
+	
+	
+	void runOnCrome2() throws InterruptedException  {
+		WebDriver driver= new ChromeDriver();
+		driver.get("https://www.saucedemo.com/");
+		driver.findElement(By.id("user-name")).sendKeys("problem_user");
+		driver.findElement(By.name("password")).sendKeys("secret_sauce");
+		driver.findElement(By.id("login-button")).click();
+		Thread.sleep(2000);
+		driver.close();
+		}
+	@Test()
+	
+	
+	void runOnCrome3() throws InterruptedException  {
+		WebDriver driver= new ChromeDriver();
+		driver.get("https://www.saucedemo.com/");
+		driver.findElement(By.id("user-name")).sendKeys("performance_glitch_user");
+		driver.findElement(By.name("password")).sendKeys("secret_sauce");
+		driver.findElement(By.id("login-button")).click();
+		Thread.sleep(2000);
+		driver.close();
+		}
+//@Test
+//
+//	void runonFirefox() throws InterruptedException {
+//		System.setProperty("Webdriver.firefox.driver", "C:\\Users\\\\Robi\\Documents\\Drivers\\geckodriver.exe");
+//		WebDriver driver= new FirefoxDriver();
+//		driver.get("https://www.saucedemo.com/");
+//		driver.findElement(By.id("user-name")).sendKeys("standard_user");
+//		driver.findElement(By.name("password")).sendKeys("secret_sauce");
+//		driver.findElement(By.id("login-button")).click();
+//		Thread.sleep(20000);
+//		driver.close();
+//	}
 }
---
+++ src/main/java/MainDemo2.java
@@ -0,0 +1,63 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class MainDemo2 {
+	private WebDriver driver;
+	
+	@BeforeMethod
+	void setpu() {
+	
+		WebDriverManager.chromedriver().setup();
+//		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Robi\\Documents\\Drivers\\chromedriver.exe");
+		driver= new ChromeDriver();
+		driver.get("https://www.saucedemo.com/");	
+	}
+	
+	@AfterMethod
+	void closing() {
+		driver.close();
+		driver.quit();
+		
+	}
+	@DataProvider(name="user-data")
+	Object[][] dataSet(){
+		Object[][]data= {
+				{"standard_user","secret_sauce"},
+				{"locked_out_user","secret_sauce"},
+				{"problem_user","secret_sauce"},
+				{"performance_glitch_user","secret_sauce"},
+		};
+		return data;
+	}
+	
+	@Test(dataProvider="user-data")
+	void runOnCrome(String username, String password) throws InterruptedException  {
+		
+		driver.findElement(By.id("user-name")).sendKeys(username);
+		driver.findElement(By.name("password")).sendKeys(password);
+		Thread.sleep(1000);
+		driver.findElement(By.id("login-button")).click();
+		Thread.sleep(2000);
+		
+		}
+	
+//@Test
+//
+//	void runonFirefox() throws InterruptedException {
+//		System.setProperty("Webdriver.firefox.driver", "C:\\Users\\\\Robi\\Documents\\Drivers\\geckodriver.exe");
+//		WebDriver driver= new FirefoxDriver();
+//		driver.get("https://www.saucedemo.com/");
+//		driver.findElement(By.id("user-name")).sendKeys("standard_user");
+//		driver.findElement(By.name("password")).sendKeys("secret_sauce");
+//		driver.findElement(By.id("login-button")).click();
+//		Thread.sleep(20000);
+//		driver.close();
+//	}
+}
